11c5c0e126da443072e41a0999ffc1fdb6e44756
TheD1TZ <ItsDitz33@gmail.com>
2022-09-22 20:55:53 +0200

    Ballz

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,12 @@
       <img src="/kimmercore-logo.png" alt="Kimmercore Logo" class="wiggle w-1/3 drop-shadow-xl">
     </section>
 
-    <section class="flex justify-center p-5 py-12 bg-gray-800">
-      <h1 class="text-4xl text-white">Latest Release</h1>
+    <section class="grid grid-cols-1 justify-center p-5 bg-gray-800">
+      <h1 class="text-4xl text-white text-center py-5">Releases</h1>
 
-      <div class="grid grid-cols-2">
+      <div class="flex justify-center">
+      <iframe style="border-radius:12px" class="w-1/2" src="https://open.spotify.com/embed/artist/14Htf1Vzv8Wrb5CfW2zmZs?utm_source=generator"
+                width="100%" height="380" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       </div>
     </section>
   </main>
