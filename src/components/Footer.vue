c20fedba257ba2442459899745c513a748407c83
TheD1TZ <ItsDitz33@gmail.com>
2022-09-22 18:54:52 +0200

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-center">
+      <p>All rights reserved®</p>
+      <p>Made by: <a href="https://github.com/TheD1TZ" target="_blank">Ditz</a></p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <section class="flex justify-center bg-neutral-900 p-20 py-72 bg-cover" style="background-image: url('https://i1.sndcdn.com/avatars-2fwVPqTrlcIJh0gj-erC7rA-t500x500.jpg')">
+      <h1 class="text-white text-7xl drop-shadow-xl">Kimmercore</h1>
+    </section>
+
+    <section class="flex justify-center p-5 py-12">
+      <h1 class="text-4xl">Latest Release</h1>
+
+      <div class="grid grid-cols-2">
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
